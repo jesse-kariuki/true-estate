a794bc7ef8f069bfa8f8249f1fe0d254a658b0bf
jesse-kariuki <kariuki.jesse@strathmore.edu>
2026-03-03 16:53:51 +0300

    Added README

--- backend/src/main/java/com/trustestate/backend/config/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.trustestate.backend.config;
-
-public class JwtConstant {
-
-    public static final String JWT_SECRET = "713e0738ac801dc3a7c300500c26d7354fd3328ca18c8ce27d810d4de6065dd6";
-    public static final String JWT_HEADER = "Authorization";
-
-
-}
